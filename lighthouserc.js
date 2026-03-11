321076909605a2646a764a78d3f060d55358de6e
Brad Knox <64992190+bknox83@users.noreply.github.com>
2026-03-11 13:25:48 -0500

    updated CI

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,8 +3,12 @@ module.exports = {
   ci: {
     collect: {
       startServerCommand: 'serve . -l 3000',
+      startServerReadyPattern: /Accepting|listen|ready/i,
       url: ['http://localhost:3000/'],
       numberOfRuns: 1,
+      settings: {
+        chromeFlags: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu'],
+      },
     },
     assert: {
       assertions: {
